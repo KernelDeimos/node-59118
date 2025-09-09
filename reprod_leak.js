cb8e54a34f3074dac0b835267598bb4b479f7bcd
KernelDeimos <eric.alex.dube@gmail.com>
2025-09-08 23:59:08 -0400

    init

---
+++ reprod_leak.js
@@ -0,0 +1,25 @@
+// Flags: --max-heap-size=16 --experimental-vm-modules
+import {SourceTextModule, createContext} from 'vm';
+import {writeHeapSnapshot} from 'v8';
+
+const context = createContext();
+
+new SourceTextModule(`export default function() { }`, {
+    identifier: 'foo',
+    context,
+});
+
+global.gc();
+
+// writeHeapSnapshot('./baseline.heapsnapshot');
+debugger;
+
+new SourceTextModule(`export default function() { }`, {
+    identifier: 'foo',
+    context,
+});
+
+global.gc();
+
+// writeHeapSnapshot('./leak.heapsnapshot');
+debugger;
