77a3920efd7b43ada9216fcc0533edef32ae0bc9
KernelDeimos <eric.alex.dube@gmail.com>
2025-09-09 00:10:04 -0400

    dev: add IIFEs to apparently no effect

--- reprod_leak.js
+++ reprod_leak.js
@@ -4,20 +4,24 @@ import {writeHeapSnapshot} from 'v8';
 
 const context = createContext();
 
-new SourceTextModule(`export default function() { }`, {
-    identifier: 'foo',
-    context,
-});
+(() => {
+    new SourceTextModule(`export default function() { }`, {
+        identifier: 'foo',
+        context,
+    });
+})();
 
 global.gc();
 
 // writeHeapSnapshot('./baseline.heapsnapshot');
 debugger;
 
-new SourceTextModule(`export default function() { }`, {
-    identifier: 'foo',
-    context,
-});
+(() => {
+    new SourceTextModule(`export default function() { }`, {
+        identifier: 'foo',
+        context,
+    });
+})();
 
 global.gc();
 
